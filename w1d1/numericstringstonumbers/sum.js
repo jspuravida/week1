ffd1c564bd5038567e42f6e8531d5efa18dea2ad
Julian Salari <julian.salari@gmail.com>
2016-10-31 21:55:03 +0000

    Adding first 3 exercises for w1d1

---
+++ w1d1/numericstringstonumbers/sum.js
@@ -0,0 +1,8 @@
+var args = process.argv;
+// This assigns a variable to the entire string array
+
+var sum = Number(args[2])+Number(args[3]);
+// This is a variable to show the inputs at index 2 and 3, converted from a string to a number.
+
+console.log(sum);
+// This shows the output.
